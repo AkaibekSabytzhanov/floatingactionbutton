98db891f82c75706d1c983672e0db2d05745d501
AkaibekSabytzhanov <akaisabyrzhanov@gmail.com>
2023-02-25 08:46:21 +0600

    animation

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  late Animation<double> _buttonAnimatedIcon;
+
+  late Animation<double> _translateButton;
+
+  bool _isExpanded = false;
+
+  @override
+  initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600))
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _buttonAnimatedIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _translateButton = Tween<double>(
+      begin: 100,
+      end: -20,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  _toggle() {
+    if (_isExpanded) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+
+    _isExpanded = !_isExpanded;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        title: const Text('Анимация'),
+        elevation: 0.0,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Transform(
+            transform: Matrix4.translationValues(
+              0.0,
+              _translateButton.value * 4,
+              0.0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.black,
+              onPressed: () {},
+              child: const Icon(
+                Icons.email,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4.translationValues(
+              0,
+              _translateButton.value * 3,
+              0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.black,
+              onPressed: () {},
+              child: const Icon(
+                Icons.call,
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4.translationValues(
+              0,
+              _translateButton.value * 2,
+              0,
+            ),
+            child: FloatingActionButton(
+              backgroundColor: Colors.black,
+              onPressed: () {},
+              child: const Icon(Icons.message),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: _toggle,
+            child: AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: _buttonAnimatedIcon,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
